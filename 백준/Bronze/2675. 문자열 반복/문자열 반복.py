379cd90e3664e741a5ff12f4de540080400375a6
choe ye jin <69586041+choeyejin12@users.noreply.github.com>
2023-06-13 23:50:11 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for _ in range(n):
+    r,s = map(str,input().split())
+    texr = ""
+    r = int(r)
+    for i in s:
+        #print(i*r,end="") 는 틀렸음 끝에 % 때문에..? 모르겟다
+        texr +=i*r
+    print(texr)        
