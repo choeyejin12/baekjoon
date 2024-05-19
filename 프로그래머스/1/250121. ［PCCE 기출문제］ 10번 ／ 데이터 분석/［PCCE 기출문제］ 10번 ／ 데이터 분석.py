1eeec00e7019a8730181a457aaef08d80dbe8f47
choe ye jin <69586041+yeh-jeans@users.noreply.github.com>
2024-05-19 23:22:52 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.38 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,23 @@
+# 코드, 제조일, 최대수량, 현재수량 순으로 구성된 데이터
+# 조건을 입력하면 만족하는 데이터만 뽑아서 정렬하려한다. 인덱스로 접근해야하려나?
+# 거의 SQL 을 코드로 구현하는 느낌이네
+# ext 에 오는 조건이 val_ext 보다 작은 데이터만 뽑고, sort_by 에 해당하는 값을 기준으로 "오름차순" 정렬하여 리턴.
+
+
+def solution(data, ext, val_ext, sort_by):
+    answer = []
+    data_name= ["code","date","maximum","remain"]
+
+    ext_idx = data_name.index(ext) # 인덱스를 반환
+    sort_idx = data_name.index(sort_by)
+
+    for i in range(len(data)) :
+        i = data[i]
+        if i[ext_idx] < val_ext :
+            answer.append(i)
+    
+    answer.sort(key=lambda x: x[sort_idx])
+    print(answer)
+    
+    
+    return answer
